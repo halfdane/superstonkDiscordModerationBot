b85839970a47facdf860999bf01b7953c159263e
halfdane <halfdane@gmx.net>
2022-04-20 09:33:48 +0200

    try to get dynamic user to work

--- src/superstonkDiscordModerationBot.py
+++ src/superstonkDiscordModerationBot.py
@@ -114,7 +114,6 @@ if __name__ == "__main__":
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[
-            logging.FileHandler("debug.log"),
             logging.StreamHandler()
         ]
     )
