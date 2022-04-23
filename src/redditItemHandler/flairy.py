35b4cb65e216cc6d046d889876dd2a02849c5c96
halfdane <halfdane@gmx.net>
2022-04-23 17:10:20 +0200

    prevent invalid flair requests from being sent to discord in the first place

--- src/redditItemHandler/flairy.py
+++ src/redditItemHandler/flairy.py
@@ -11,9 +11,16 @@ from disnake.utils import escape_markdown
 class Flairy(Handler, Reaction):
     emoji = '🧚'
 
+    _flairy_command = ".*!\s*FL?AIRY\s*!"
+    _flairy_text = "\s*(.*?)"
+    _valid_colors = "(?:\s+(red|blue|pink|yellow|green|black))?\s*"
+    _last_word = "(\w*)"
+
     _flairy_detect_user_flair_change = \
-        re.compile(r".*!\s*FL?AIRY\s*!\s*(.*?)(?:\s+(red|blue|pink|yellow|green|black))?\s*$",
-                   re.IGNORECASE | re.MULTILINE | re.DOTALL)
+        re.compile(rf"{_flairy_command}{_flairy_text}{_valid_colors}$", re.IGNORECASE | re.MULTILINE | re.DOTALL)
+
+    _flairy_detect_last_word = \
+        re.compile(rf"{_flairy_command}{_flairy_text}{_last_word}$", re.IGNORECASE | re.MULTILINE | re.DOTALL)
 
     _templates = {"red": "0446bc04-91c0-11ec-8118-ce042afdde96",
                   "blue": "6e40ab4c-f3cd-11eb-889e-ae4cdf00ff3b",
@@ -29,11 +36,31 @@ class Flairy(Handler, Reaction):
         Reaction.__init__(self, bot)
 
     async def take(self, comment):
-        if self._flairy_detect_user_flair_change.match(comment.body) \
+        body = getattr(comment, 'body', "")
+        if (flairy := self._flairy_detect_user_flair_change.match(body)) \
                 and comment.author not in self.bot.moderators \
                 and comment.author.name not in ["Roid_Rage_Smurf"] \
                 and await self.is_new_item(self.bot.flairy_channel, comment):
-            self._logger.info(f"Sending flair request {comment}")
+
+            flair_text = flairy.group(1)
+            if len(flair_text) > 63:
+                comment_from_flairies_view = await self.bot.flairy_reddit.comment(comment.id, fetch=False)
+                message = "(ノಠ益ಠ)ノ彡┻━┻ THE FLAIR TEXT IS TOO LONG!   \nPlease use less than 64 unicode characters"
+                self._logger.info(f"Too long: https://www.reddit.com{comment.permalink}")
+                await comment_from_flairies_view.reply(message)
+                return
+
+            last_word = self._flairy_detect_last_word.match(body).group(2)
+            if last_word.lower() in ["orange", "grey", "gray", "purple", "white"]:
+                comment_from_flairies_view = await self.bot.flairy_reddit.comment(comment.id, fetch=False)
+                message = f"(ノಠ益ಠ)ノ彡┻━┻ {last_word.upper()} IS NOT A VALID COLOR!   \n" \
+                          f"Valid colors are `red`, `blue`, `pink`, `yellow`, `green` or `black`.   \n" \
+                          f"I'm making the change, so if that's not what you want " \
+                          f"you have to summon me again."
+                self._logger.info(f"Wrong color: https://www.reddit.com{comment.permalink}")
+                await comment_from_flairies_view.reply(message)
+
+            self._logger.info(f"Sending flair request {comment} {flair_text}")
             url = f"https://www.reddit.com{comment.permalink}"
             e = disnake.Embed(
                 url=url,
@@ -45,38 +72,29 @@ class Flairy(Handler, Reaction):
             await self.bot.add_reactions(msg)
 
     async def handle_reaction(self, message, emoji, user, channel):
+        if channel != self.bot.flairy_channel:
+            return
+
         comment = await self.bot.get_item(message)
         body = getattr(comment, 'body', "")
-        subreddit = self.bot.subreddit
-        flairy_reddit = self.bot.flairy_reddit
-        mods = self.bot.moderators
+        flairy = self._flairy_detect_user_flair_change.match(body)
+        await self.flair_user(comment, flairy.group(1), flairy.group(2))
+        await self.bot.handle_reaction(message, "✅", user, channel)
 
-        user_to_be_flaired = comment.author
-        flairy_user = await flairy_reddit.user.me()
-        if (flairy := self._flairy_detect_user_flair_change.match(body)) \
-                     and user_to_be_flaired not in mods \
-                     and user_to_be_flaired != flairy_user:
-            await self.flair_user(subreddit, comment, flairy_reddit, user_to_be_flaired, flairy.group(1),
-                                  flairy.group(2))
-            await self.bot.handle_reaction(message, "✅", user, channel)
-
-    async def flair_user(self, subreddit, comment, flairy_reddit, user_to_be_flaired, flair_match, color_match):
+    async def flair_user(self, comment, flair_match, color_match):
         flair_text = flair_match.strip()
         color = (color_match or self._default_color).lower().strip()
-        if color not in self._templates.keys():
-            message = f"Cowardly refusing to use [{color}] as a color!"
-            log_message = f"Wrong color [{color}]: not changing {user_to_be_flaired}'s flair"
-        elif len(flair_text) > 63:
-            message = f"(ノಠ益ಠ)ノ彡┻━┻ THE FLAIR TEXT IS TOO LONG!"
-            log_message = f"Too long: Not changing {user_to_be_flaired}'s flair to [{flair_text}]"
-        else:
-            template = self._templates[color]
-            previous_flair = getattr(comment, 'author_flair_text', "")
-            log_message = f"[{previous_flair}] => [{flair_text}] with template {template} for the color {color}"
-            await subreddit.flair.set(redditor=user_to_be_flaired, text=flair_text, flair_template_id=template)
-            message = rf"(✿\^‿\^)━☆ﾟ.*･｡ﾟ `{flair_text}`"
+        template = self._templates[color]
+        previous_flair = getattr(comment, 'author_flair_text', "")
+        log_message = f"[{previous_flair}] => [{flair_text}] with template {template} for the color {color}"
+        subreddit_from_flairies_view = await self.bot.flairy_reddit.subreddit("Superstonk")
+        await subreddit_from_flairies_view.flair.set(
+            redditor=comment.author,
+            text=flair_text,
+            flair_template_id=template)
+        message = rf"(✿\^‿\^)━☆ﾟ.*･｡ﾟ `{flair_text}`"
         self._logger.info(log_message)
-        comment_from_flairies_view = await flairy_reddit.comment(comment.id, fetch=False)
+        comment_from_flairies_view = await self.bot.flairy_reddit.comment(comment.id, fetch=False)
         await comment_from_flairies_view.upvote()
         await comment_from_flairies_view.reply(message)
 
