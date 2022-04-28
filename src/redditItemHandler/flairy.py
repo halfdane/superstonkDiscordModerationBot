e465769f02cd34bb046fd9dad3295ed8b416901d
halfdane <halfdane@gmx.net>
2022-04-28 11:33:54 +0200

    be able to clear flairs or append seals

--- src/redditItemHandler/flairy.py
+++ src/redditItemHandler/flairy.py
@@ -1,3 +1,4 @@
+import logging
 import re
 
 import asyncpraw.models
@@ -26,25 +27,42 @@ class Flairy(Handler, Reaction):
         Handler.__init__(self, bot)
         Reaction.__init__(self, bot)
 
-        self._flairy_command = r".*!\s*FL?AIRY\s*!"
+        self.flairy_command_detection = r".*!\s*FL?AIRY"
+        self.flair_command = rf"{self.flairy_command_detection}\s*!"
+
         self._flairy_text = r"\s*(.*?)"
         self._valid_colors = fr"(?:\s+({'|'.join(self._templates.keys())}))?\s*"
-        self._last_word = r"(\w*)"
-        flags = re.IGNORECASE | re.MULTILINE | re.DOTALL
+
+        self.flags = re.IGNORECASE | re.MULTILINE | re.DOTALL
+
+        self.detect_flairy_command = \
+            re.compile(rf"{self.flairy_command_detection}", self.flags)
+
         self._flairy_detect_user_flair_change = \
-            re.compile(rf"{self._flairy_command}{self._flairy_text}{self._valid_colors}$", flags)
-        self._flairy_detect_last_word = \
-            re.compile(rf"{self._flairy_command}{self._flairy_text}{self._last_word}$", flags)
+            re.compile(rf"{self.flair_command}{self._flairy_text}{self._valid_colors}$", self.flags)
+
+        self._commands = [
+            ClearCommand(self),
+            SealmeCommand(self, self._templates[self._default_color]),
+            RandomFlairCommand(self),
+            WrongColorCommand(self)
+        ]
 
     async def take(self, comment):
         body = getattr(comment, 'body', "")
-        if (flairy := self._flairy_detect_user_flair_change.match(body)) \
+        if (self.detect_flairy_command.match(body)) \
                 and comment.author not in self.bot.moderators \
                 and comment.author.name not in ["Roid_Rage_Smurf"]:
+
             if await self._was_recently_posted(comment, self.bot.flairy_channel):
                 self._logger.info(f"skipping over recently handled flair request {self.permalink(comment)}")
                 return
 
+            for command in self._commands:
+                if await command.handled(body, comment):
+                    return
+
+            flairy = self._flairy_detect_user_flair_change.match(body)
             flair_text = flairy.group(1)
             if len(flair_text) > 63:
                 comment_from_flairies_view = await self.bot.flairy_reddit.comment(comment.id, fetch=False)
@@ -57,20 +75,6 @@ class Flairy(Handler, Reaction):
                 self._logger.info(f"Silently refusing to grant flair with restricted content: {flair_text}")
                 return
 
-            if len(flair_text) == 0:
-                await self._bestow_random_flair(comment)
-                return
-
-            last_word = self._flairy_detect_last_word.match(body).group(2)
-            if last_word.lower() in ["orange", "grey", "gray", "purple", "white"]:
-                comment_from_flairies_view = await self.bot.flairy_reddit.comment(comment.id, fetch=False)
-                message = f"(ノಠ益ಠ)ノ彡┻━┻ {last_word.upper()} IS NOT A VALID COLOR!   \n" \
-                          f"Valid colors are {', '.join(self._templates.keys())}.   \n" \
-                          f"I'm making the change, so if that's not what you want " \
-                          f"you have to summon me again."
-                self._logger.info(f"Wrong color: {self.permalink(comment)}")
-                await comment_from_flairies_view.reply(message)
-
             self._logger.info(f"Sending flair request {comment} {flair_text}")
             url = self.permalink(comment)
             e = disnake.Embed(
@@ -81,34 +85,6 @@ class Flairy(Handler, Reaction):
             msg = await self.bot.flairy_channel.send(embed=e)
             await msg.add_reaction(self.emoji)
             await self.bot.add_reactions(msg)
-
-    async def _bestow_random_flair(self, comment):
-        _emojis = ['🏴‍☠', '💪', '💎', '🎊', '🌕',
-                   '🦍🚀', '🦍', '💎🙌🏻', '🎮🛑',
-                   '♾️', '🐵', '💙', '🍦💩🪑'
-                   ]
-
-        _flairs = ["TOMORROW!", "Hola", "I'm here for the memes", "es mucho", "We're in the endgame now", "Gamecock",
-                   "FUCK YOU PAY ME", "Locked and loaded ", "Power to the Players", "Nothin But Time", "GME to the Moon! ",
-                   "Infinite Risk ", "Hang in There! ", "Power to the Players ", "Power to the Creators ",
-                   "Probably nothing", "Gimme me my money ", "GME ", "Apes together strong", "before the split",
-                   "Gamestop 4U", "Casual lurker until MOASS", "GMERICA ", "GME ", "Superstonk Ape", "GME to the Moon!",
-                   "Pepperidge Farm remembers", "LOVE GME ", "SuperApe ", "What’s an exit strategy", "C.R.E.A.M",
-                   "ZEN APE ", "No Cell No Sell", "GameStop", "Power to the Players ", "Crayon Sniffer ", "Mods are sus",
-                   "DEEP FUCKING VALUE ", "Fuck no I’m not selling my GME!", "Fuck Citadel", "I just love the stock ",
-                   "FUD is the Mind-Killer", "No target, just up!", "We are in a completely fraudulent system ",
-                   "Get rich or die buyin’", "GME go Brrrr ", "Apes together strong ", "wen moon", "I SAID WE GREEN TODAY",
-                   "Buy now, ask questions later ", "I am not a cat", "I like the stock. ", "Just Like the Stonk",
-                   "Bullish"]
-
-        emojis = random.sample(_emojis, 2)
-        flair_text = f"{emojis[0]} {random.sample(_flairs, 1)[0]} {emojis[1]}"
-        color = random.sample(list(self._templates.keys()), 1)[0]
-        message = "(ノಠ益ಠ)ノ彡┻━┻ YOU DIDN'T ASK FOR A FLAIR!\n\n " \
-                  "Let me show you how to do it:  \n\n" \
-                  f"     !FLAIRY!{flair_text}{color}\n\n...\n\n...\n\n"
-        self._logger.info(f"Randomly assigning: {self.permalink(comment)}")
-        await self.flair_user(comment, flair_text, color, message)
 
     async def handle_reaction(self, message, emoji, user, channel):
         if channel != self.bot.flairy_channel:
@@ -123,10 +99,13 @@ class Flairy(Handler, Reaction):
             await self.bot.handle_reaction(message, "✅", user, channel)
             await message.edit(content="Flair request was removed in the meantime")
 
-    async def flair_user(self, comment, flair_match, color_match, message=""):
-        flair_text = flair_match.strip()
-        color = (color_match or self._default_color).lower().strip()
-        template = self._templates[color]
+    async def flair_user(self, comment, flair_text, flair_color=None, template=None, message=""):
+        if (not flair_color or template) and (flair_color or not template):
+            raise Exception("give either color or color_template")
+
+        flair_text = flair_text.strip()
+        color = (flair_color or self._default_color).lower().strip()
+        template = (template or self._templates[color])
         previous_flair = getattr(comment, 'author_flair_text', "")
         log_message = f"[{previous_flair}] => [{flair_text}] with template {template} for the color {color}"
         subreddit_from_flairies_view = await self.bot.flairy_reddit.subreddit("Superstonk")
@@ -143,3 +122,124 @@ class Flairy(Handler, Reaction):
     def description(self):
         return "Accept the flair request. The flairy will take care of the rest."
 
+
+class RandomFlairCommand:
+
+    def __init__(self, flairy):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._flairy = flairy
+        self._random_flair_command = \
+            re.compile(rf"{self._flairy.flairy_command_detection}\s*!$", self._flairy.flags)
+
+    async def handled(self, body, comment):
+        if self._random_flair_command.match(body):
+            await self._bestow_random_flair(comment)
+            return True
+
+        return False
+
+    async def _bestow_random_flair(self, comment):
+        _emojis = ['🏴‍☠', '💪', '💎', '🎊', '🌕',
+                   '🦍🚀', '🦍', '💎🙌🏻', '🎮🛑',
+                   '♾️', '🐵', '💙', '🍦💩🪑'
+                   ]
+
+        _flairs = ["TOMORROW!", "Hola", "I'm here for the memes", "es mucho", "We're in the endgame now", "Gamecock",
+                   "FUCK YOU PAY ME", "Locked and loaded ", "Power to the Players", "Nothin But Time",
+                   "GME to the Moon! ",
+                   "Infinite Risk ", "Hang in There! ", "Power to the Players ", "Power to the Creators ",
+                   "Probably nothing", "Gimme me my money ", "GME ", "Apes together strong", "before the split",
+                   "Gamestop 4U", "Casual lurker until MOASS", "GMERICA ", "GME ", "Superstonk Ape", "GME to the Moon!",
+                   "Pepperidge Farm remembers", "LOVE GME ", "SuperApe ", "What’s an exit strategy", "C.R.E.A.M",
+                   "ZEN APE ", "No Cell No Sell", "GameStop", "Power to the Players ", "Crayon Sniffer ",
+                   "Mods are sus",
+                   "DEEP FUCKING VALUE ", "Fuck no I’m not selling my GME!", "Fuck Citadel", "I just love the stock ",
+                   "FUD is the Mind-Killer", "No target, just up!", "We are in a completely fraudulent system ",
+                   "Get rich or die buyin’", "GME go Brrrr ", "Apes together strong ", "wen moon",
+                   "I SAID WE GREEN TODAY",
+                   "Buy now, ask questions later ", "I am not a cat", "I like the stock. ", "Just Like the Stonk",
+                   "Bullish"]
+
+        emojis = random.sample(_emojis, 2)
+        flair_text = f"{emojis[0]} {random.sample(_flairs, 1)[0]} {emojis[1]}"
+        color = random.sample(list(self._flairy._templates.keys()), 1)[0]
+        message = "(ノಠ益ಠ)ノ彡┻━┻ YOU DIDN'T ASK FOR A FLAIR!\n\n " \
+                  "Let me show you how to do it:  \n\n" \
+                  f"     !FLAIRY!{flair_text}{color}\n\n...\n\n...\n\n"
+        self._logger.info(f"Randomly assigning: {self._flairy.permalink(comment)}")
+
+        await self._flairy.flair_user(
+            comment=comment,
+            flair_text=flair_text,
+            color=color,
+            message=message)
+
+
+class SealmeCommand:
+    def __init__(self, flairy, default_template):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._flairy = flairy
+        self._sealme_command = \
+            re.compile(rf"{self._flairy.flairy_command_detection}:SEALME\s*!", self._flairy.flags)
+        self._default_template = default_template
+
+    async def handled(self, body, comment):
+        if self._sealme_command.match(body):
+            current_flair = getattr(comment, 'author_flair_text', "")
+            current_template = getattr(comment, 'author_flair_template_id', self._default_template)
+            message = 'Witness meee /u/Justind123  \n\n'
+            await self._flairy.flair_user(
+                comment=comment,
+                flair_text=current_flair+'🦭',
+                template=current_template,
+                message=message)
+
+            return True
+
+        return False
+
+
+class ClearCommand:
+    def __init__(self, flairy):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._flairy = flairy
+
+        self._reset_command = \
+            re.compile(rf"{self._flairy.flairy_command_detection}:CLEARME\s*!", self._flairy.flags)
+
+    async def handled(self, body, comment):
+        if self._reset_command.match(body):
+            message = 'Clearing the flair as requested  \n\n' + r'(✿\^‿\^)━☆ﾟ.*･｡ﾟ '
+            self._logger.info(f"Clearing flair: {self._flairy.permalink(comment)}")
+
+            subreddit_from_flairies_view = await self._flairy.bot.flairy_reddit.subreddit("Superstonk")
+            await subreddit_from_flairies_view.flair.delete(redditor=comment.author)
+            comment_from_flairies_view = await self._flairy.bot.flairy_reddit.comment(comment.id, fetch=False)
+            await comment_from_flairies_view.reply(message)
+            return True
+
+        return False
+
+
+class WrongColorCommand:
+    def __init__(self, flairy):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._flairy = flairy
+
+        self._last_word = r"(\w*)"
+
+        self._detect_last_word = \
+            re.compile(rf"{self._flairy.flair_command}.*?{self._last_word}$", self._flairy.flags)
+
+    async def handled(self, body, comment):
+        last_word = self._detect_last_word.match(body).group(1)
+        if last_word.lower() in ["orange", "grey", "gray", "purple", "white"]:
+            comment_from_flairies_view = await self._flairy.bot.flairy_reddit.comment(comment.id, fetch=False)
+            message = f"(ノಠ益ಠ)ノ彡┻━┻ {last_word.upper()} IS NOT A VALID COLOR!   \n" \
+                      f"Valid colors are {', '.join(self._flairy._templates.keys())}.   \n" \
+                      f"I'm making the change, so if that's not what you want " \
+                      f"you have to summon me again."
+            self._logger.info(f"Wrong color: {self._flairy.permalink(comment)}")
+            await comment_from_flairies_view.reply(message)
+
+        return False
--- test/test_flairy.py
+++ test/test_flairy.py
@@ -14,6 +14,8 @@ class TestFlairyRegex:
         mock_bot.flairy_channel = MagicMock()
         mock_bot.flairy_channel.send = AsyncMock()
         mock_bot.flairy_channel.history.filter.__aiter__.return_value = []
+        mock_bot.flairy_reddit.subreddit = AsyncMock()
+        mock_bot.flairy_reddit.subreddit.flair.delete = AsyncMock()
 
         mock_bot.is_forbidden_comment_message.return_value = False
 
@@ -21,6 +23,7 @@ class TestFlairyRegex:
         mock_comment.id = "some id"
         mock_comment.body = "!FLAIRY! some flair"
         mock_comment.author.name = ""
+        mock_comment.author_flair_text = "existing flair"
 
     @patch('superstonkDiscordModerationBot.SuperstonkModerationBot', autospec=True)
     @patch('asyncpraw.models.Comment')
@@ -98,7 +101,103 @@ class TestFlairyRegex:
 
         # then
         testee.flair_user.assert_called_once()
-        assert testee.flair_user.call_args[0][0] == mock_comment
-        assert len(testee.flair_user.call_args[0][1]) > 2
-        assert len(testee.flair_user.call_args[0][2]) > 2
-        assert "YOU DIDN'T ASK FOR A FLAIR" in testee.flair_user.call_args[0][3]
+        _, kwargs = testee.flair_user.call_args
+
+        assert kwargs['comment'] == mock_comment
+        assert len(kwargs['flair_text']) > 2
+        assert len(kwargs['color']) > 2
+        assert "YOU DIDN'T ASK FOR A FLAIR" in kwargs['message']
+        assert "template" not in kwargs
+
+    @patch('superstonkDiscordModerationBot.SuperstonkModerationBot', autospec=True)
+    @patch('asyncpraw.models.Comment')
+    @pytest.mark.asyncio
+    async def test_wrong_color_results_in_additional_message(self, mock_comment, mock_bot):
+        # given
+        self.default_comment(mock_comment)
+        mock_comment.body = "!FLAIRY! requesting wrong color: orange"
+
+        self.default_bot(mock_bot)
+
+        # when
+        testee = Flairy(mock_bot)
+        await testee.take(mock_comment)
+
+        # then
+        flairy_comment = mock_bot.flairy_reddit.comment.return_value
+        flairy_comment.reply.assert_called_once()
+        assert "ORANGE IS NOT A VALID COLOR!" in flairy_comment.reply.call_args[0][0]
+        mock_bot.flairy_channel.send.assert_called_once()
+
+    @patch('superstonkDiscordModerationBot.SuperstonkModerationBot', autospec=True)
+    @patch('asyncpraw.models.Comment')
+    @pytest.mark.asyncio
+    async def test_flair_clearing(self, mock_comment, mock_bot):
+        # given
+        self.default_comment(mock_comment)
+        mock_comment.body = "!FLAIRY:CLEARME! whatever happens here"
+
+        self.default_bot(mock_bot)
+
+        # when
+        testee = Flairy(mock_bot)
+        await testee.take(mock_comment)
+
+        # then
+        flairy_comment = mock_bot.flairy_reddit.comment.return_value
+        flairy_comment.reply.assert_called_once()
+        assert "Clearing the flair as requested" in flairy_comment.reply.call_args[0][0]
+
+        mock_bot.flairy_channel.send.assert_not_called()
+
+        mock_bot.flairy_reddit.subreddit.return_value.flair.delete.assert_called_once()
+
+    @patch('superstonkDiscordModerationBot.SuperstonkModerationBot', autospec=True)
+    @patch('asyncpraw.models.Comment')
+    @pytest.mark.asyncio
+    async def test_flair_seal_appending(self, mock_comment, mock_bot):
+        # given
+        self.default_comment(mock_comment)
+        mock_comment.body = "!FLAIRY:SEALME! whatever happens here"
+        mock_comment.author_flair_text = "some flair"
+        mock_comment.author_flair_template_id = "some template id"
+
+        self.default_bot(mock_bot)
+
+        # when
+        testee = Flairy(mock_bot)
+        testee.flair_user = AsyncMock()
+        await testee.take(mock_comment)
+
+        # then
+        testee.flair_user.assert_called_once()
+        _, kwargs = testee.flair_user.call_args
+
+        assert "Witness meee /u/Justind123" in kwargs['message']
+        assert "some flair🦭" in kwargs['flair_text']
+        assert "some template id" in kwargs['template']
+        assert "color" not in kwargs
+
+        mock_bot.flairy_channel.send.assert_not_called()
+
+        mock_bot.flairy_reddit.subreddit.return_value.flair.delete.assert_not_called()
+
+    @patch('superstonkDiscordModerationBot.SuperstonkModerationBot', autospec=True)
+    @patch('asyncpraw.models.Comment')
+    @pytest.mark.asyncio
+    async def test_actual_user_flairing(self, mock_comment, mock_bot):
+        # given
+        self.default_comment(mock_comment)
+        mock_comment.body = "!FLAIRY:SEALME! whatever happens here"
+        mock_comment.author_flair_text = "some flair"
+        mock_comment.author_flair_template_id = "some template id"
+
+        self.default_bot(mock_bot)
+
+        # when
+        testee = Flairy(mock_bot)
+        with pytest.raises(Exception):
+            await testee.flair_user(mock_comment, "some flair", flair_color="green", template="553", message="hello")
+
+        # then
+
