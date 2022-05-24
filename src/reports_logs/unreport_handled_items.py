f6256e6dc21c1738b6ee2a10da1fd41b1f4ed3d1
halfdane <halfdane@gmx.net>
2022-05-24 11:25:06 +0200

    clean up reporting channel on start

--- src/reports_logs/unreport_handled_items.py
+++ src/reports_logs/unreport_handled_items.py
@@ -1,18 +1,21 @@
 import logging
+from datetime import datetime
 
 from helper.links import permalink
 
 
 class HandledItemsUnreporter:
 
-    def __init__(self, superstonk_subreddit=None, report_channel=None, **kwargs):
+    def __init__(self, superstonk_subreddit=None, report_channel=None, discord_bot_user=None, **kwargs):
         self._logger = logging.getLogger(self.__class__.__name__)
         self.superstonk_subreddit = superstonk_subreddit
         self.report_channel = report_channel
+        self.discord_bot_user = discord_bot_user
 
     async def on_ready(self, scheduler, **kwargs):
         self._logger.info(f"Scheduling cleanup of handled reports")
         scheduler.add_job(self.unreport_items, "cron", minute="*")
+        scheduler.add_job(self.remove_handled_items, "cron", day="*", next_run_time=datetime.now())
 
     async def unreport_items(self):
         unreport_actions = ['spamlink', 'removelink', 'approvelink', 'spamcomment', 'removecomment', 'approvecomment']
@@ -23,4 +26,26 @@ class HandledItemsUnreporter:
                 .filter(lambda r: len(getattr(r, 'embeds', [])) > 0 and r.embeds[0].url in handled_urls):
             await message.delete()
             self._logger.info(f'removed report for {message.embeds[0].url}')
+
+    async def remove_handled_items(self):
+        def __was_confirmed(message):
+            bot_message = message.author.id == self.discord_bot_user.id
+            additional_reactions = [r.emoji for r in message.reactions if r.count >= 2]
+            confirmed = '✅' in additional_reactions
+            return bot_message and confirmed
+
+        run_once_more = True
+        while run_once_more:
+            run_once_more = False
+            async for message in self.report_channel \
+                    .history(limit=200) \
+                    .filter(__was_confirmed):
+                await message.delete()
+                run_once_more = True
+                self._logger.info(f'removed report for {message.embeds[0].url}')
+
+        self._logger.info("Cleaned up channel")
+
+
+
 
