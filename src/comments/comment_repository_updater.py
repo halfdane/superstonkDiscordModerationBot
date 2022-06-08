01343eca77cd4da3297e5fc2bfb46d65992434c7
halfdane <halfdane@gmx.net>
2022-06-08 16:14:37 +0200

    report average score

--- src/comments/comment_repository_updater.py
+++ src/comments/comment_repository_updater.py
@@ -1,15 +1,20 @@
 import logging
 from datetime import timedelta, datetime
 
+import disnake
+
 from comments.comment_repository import Comments
 
 
 class CommentRepositoryUpdater:
 
-    def __init__(self, comment_repo: Comments = None, readonly_reddit=None, **kwargs):
+    def __init__(self, comment_repo: Comments = None, readonly_reddit=None,
+                 add_reactions_to_discord_message=None, report_comments_channel=None, **kwargs):
         self._logger = logging.getLogger(self.__class__.__name__)
         self.persist_comments = comment_repo
         self.readonly_reddit = readonly_reddit
+        self.add_reactions_to_discord_message = add_reactions_to_discord_message
+        self.report_comments_channel = report_comments_channel
 
     async def on_ready(self, scheduler, **kwargs):
         self._logger.info(f"Ready to update comments")
@@ -25,3 +30,8 @@ class CommentRepositoryUpdater:
         self._logger.info(f"Storing updated info for {len(comments_of_last_hour)} comments from the last hour")
         await self.persist_comments.store(comments_of_last_hour)
 
+        avg_score = sum([c.score for c in comments_of_last_hour]) / len([comments_of_last_hour])
+        message = f"average score of {len(comments_of_last_hour)} in last hour: {avg_score}"
+        await self.report_comments_channel.send(embed=(
+            disnake.Embed(colour=disnake.Colour(0).from_rgb(207, 206, 255), description=result)))
+        await self.add_reactions_to_discord_message(message)
