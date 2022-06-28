16b5c8abc79f53198ff08079eb95b7beba0a8634
halfdane <halfdane@gmx.net>
2022-06-28 11:04:11 +0200

    fetch posts correctly from database :/

--- src/posts/post_url_limiter.py
+++ src/posts/post_url_limiter.py
@@ -43,7 +43,7 @@ class UrlPostLimiter(Handler):
             limit = 3
 
         p_fids = [p.id for p in posts_with_same_url]
-        posts_with_same_url = [post async for post in self.post_repo.fetch(ids=p_fids)]
+        posts_with_same_url = await self.post_repo.fetch(ids=p_fids)
 
         count_of_posts = len(posts_with_same_url)
         self._logger.info(f"url {url} - amount of times it was posted: {count_of_posts}")
