6949b53a6546396a7581dc3c7f29967764f9ee97
halfdane <halfdane@gmx.net>
2022-07-06 10:51:32 +0200

    activate removal of reported comments from moderated posts

--- src/reports_logs/reported_comments_remover.py
+++ src/reports_logs/reported_comments_remover.py
@@ -5,11 +5,12 @@ from helper.links import permalink
 
 class ReportedCommentsRemover:
 
-    def __init__(self, superstonk_subreddit, qvbot_reddit, send_discord_message, **kwargs):
+    def __init__(self, superstonk_subreddit, qvbot_reddit, send_discord_message, is_live_environment, **kwargs):
         self._logger = logging.getLogger(self.__class__.__name__)
         self.superstonk_subreddit = superstonk_subreddit
         self.qvbot_reddit = qvbot_reddit
         self.send_discord_message = send_discord_message
+        self.is_live_environment = is_live_environment
 
     def wot_doing(self):
         return "Remove reported comments if the post was removed or locked."
@@ -26,8 +27,18 @@ class ReportedCommentsRemover:
         async for post in self.qvbot_reddit.info(handled_post_ids):
             await post.load()
             await post.comments.replace_more(limit=None)
-            comments = [permalink(c) for c in post.comments if len(getattr(c, 'user_reports', [])) > 0]
+            comments = [permalink(c) for c in post.comments if
+                        len(getattr(c, 'user_reports', [])) > 0 or len(getattr(c, 'mod_reports', [])) > 0]
+
+            for comment in comments:
+                if self.is_live_environment:
+                    item_from_qvbot_view = await self.qvbot_reddit.submission(comment.id, fetch=False)
+                    await item_from_qvbot_view.mod.remove(spam=False, mod_note="Cleaning up")
+                else:
+                    self._logger.info("Feature isn't active, so I'm not removing anything.")
 
             if len(comments) > 0:
-                await self.send_discord_message(description_beginning=", ".join(comments))
+                await self.send_discord_message(
+                    description_beginning="CLEANED UP comments from moderated post",
+                    fields={'comments': comments})
 
