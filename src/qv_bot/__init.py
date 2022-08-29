7b29b3ec3f30aa0b444abd081ffa48be6c773a34
halfdane <halfdane@gmx.net>
2022-08-29 16:08:15 +0200

    await qv comment before using it

--- src/qv_bot/__init.py
+++ src/qv_bot/__init.py
@@ -4,6 +4,6 @@ from helper.item_helper import author
 async def get_qv_comment(qvbot_reddit, post):
     qv_user = await qvbot_reddit.user.me()
     if len(post.comments) > 0 and post.comments[0].stickied and author(post.comments[0]) == qv_user.name:
-        return qvbot_reddit.comment(id=post.comments[0].id)
+        return await qvbot_reddit.comment(id=post.comments[0].id)
 
     return None
