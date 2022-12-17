25312f50c60d7ea09ca17d166aea01f78b16f37d
halfdane <halfdane@gmx.net>
2022-12-17 18:08:38 +0100

    oops, mods are redditor too, not strings

--- src/comments/mod_tagger.py
+++ src/comments/mod_tagger.py
@@ -16,7 +16,7 @@ class ModTagger(Handler):
         body = getattr(item, 'body', "")
 
         for mod in self.superstonk_moderators_strict:
-            if mod in body:
+            if mod.name in body:
                 await self.send_discord_message(item=item, description_beginning=f"Moderator Tag")
                 break
 
