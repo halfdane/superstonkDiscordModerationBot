701711f4e8c32c96355fa09f21f8c1bef16c7da6
halfdane <halfdane@gmx.net>
2022-12-17 18:09:36 +0100

    await response from repository

--- src/trolls/troll_reporter.py
+++ src/trolls/troll_reporter.py
@@ -14,7 +14,7 @@ class TrollReporter(Handler):
         return "report potential trolls when they get active in superstonk"
 
     async def take(self, item):
-        troll_source = self.troll_repository.get_troll_source(item)
+        troll_source = await self.troll_repository.get_troll_source(item)
         if troll_source is not None:
             await item.report(f"Potential Troll: user has posted or commented in {troll_source}")
             await self.send_discord_message(item=item, description_beginning=f"Potential Troll from {troll_source}")
