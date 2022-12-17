b1433542dee663ae411b7676d2396988b388dd44
halfdane <halfdane@gmx.net>
2022-12-17 18:28:26 +0100

    ignore tagged  mods

--- src/comments/mod_tagger.py
+++ src/comments/mod_tagger.py
@@ -1,5 +1,12 @@
 from reddit_item_handler import Handler
 
+IGNORE_BOTS = [
+    "Automod",
+    "Superstonk_QV",
+    "Superstonk-Flairy"
+]
+
+
 class ModTagger(Handler):
 
     def __init__(self, readonly_reddit, superstonk_subreddit, send_discord_message, superstonk_moderators_strict, **_):
@@ -9,14 +16,16 @@ class ModTagger(Handler):
         self.superstonk_moderators_strict = superstonk_moderators_strict
         self.send_discord_message = send_discord_message
 
+        self.mods = [mod.name.lower() for mod in superstonk_moderators_strict if mod not in IGNORE_BOTS]
+
     def wot_doing(self):
         return "report content where people try to tag mods"
 
     async def take(self, item):
         body = getattr(item, 'body', "")
 
-        for mod in self.superstonk_moderators_strict:
-            if mod.name.lower() in body.lower():
+        for mod in self.mods:
+            if mod in body.lower():
                 await self.send_discord_message(item=item, description_beginning=f"Moderator Tag")
                 break
 
